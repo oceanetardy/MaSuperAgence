fd3c0512e9a7bf8e30143de36c602de65f7f9ec1
Prénom <email@example.com>
2022-01-10 17:38:51 +0100

    video 5 validation données

--- src/Entity/Property.php
+++ src/Entity/Property.php
@@ -6,10 +6,14 @@ use App\Repository\PropertyRepository;
 use DateTime;
 use Doctrine\ORM\Mapping as ORM;
 use Cocur\Slugify\Slugify;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use function number_format;
 
 
 #[ORM\Entity(repositoryClass: PropertyRepository::class)]
+#[UniqueEntity("title")]
+
 class Property
 {
     const HEAT = [
@@ -23,6 +27,9 @@ class Property
     #[ORM\Column(type: 'integer')]
     private $id;
 
+
+    #[Assert\Length(min: '5')]
+    #[Assert\Length(max: '255')]
     #[ORM\Column(type: 'string', length: 255)]
     private $title;
 
@@ -30,6 +37,8 @@ class Property
     private $description;
 
     #[ORM\Column(type: 'integer')]
+    #[Assert\Range(min: '10')]
+    #[Assert\Range(max: '400')]
     private $surface;
 
     #[ORM\Column(type: 'integer')]
@@ -54,6 +63,7 @@ class Property
     private $address;
 
     #[ORM\Column(type: 'string', length: 255)]
+    #[Assert\Regex("/[0-9]{5}$/")]
     private $postal_code;
 
     #[ORM\Column(type: 'boolean')]
